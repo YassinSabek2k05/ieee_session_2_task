dbd6cbd9d4084cd05923510ea2f71ef799c7eb92
Yassin Ahmed Sabek <99022060+YassinSabek2k05@users.noreply.github.com>
2025-12-09 18:11:20 +0200

    implemented student class with properties and methods, including display functionality

--- student.java
+++ student.java
@@ -12,3 +12,73 @@ it should also have the following methods:
 - a getter and setter for each property
 - main method to create a student object (with your information) and display its information
 */ 
+public class student{
+    String name;
+    int age;
+    int grade;
+    int studentID;
+    String major;
+    double gpa;
+    public student(String name, int age, int grade, int studentID, String major, double gpa){
+        this.name= name;
+        this.age=age;
+        this.grade=grade;
+        this.studentID=studentID;
+        this.major= major;
+        this.gpa=gpa;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+    public int getGrade() {
+        return grade;
+    }
+    
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+    
+    public int getStudentID() {
+        return studentID;
+    }
+    
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+    
+    public String getMajor() {
+        return major;
+    }
+    
+    public void setMajor(String major) {
+        this.major = major;
+    }
+    
+    public double getGpa() {
+        return gpa;
+    }
+    
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+    public void display(){
+        System.out.println(this.name+", Age:"+this.age+", Grade:"+this.grade+", Student ID:"+this.studentID+", Major:"+this.major);
+    }
+    public static void main(String[] args) {
+        student s = new student("Yassin", 20, 1, 1553, "MET", 1.4);
+        s.display();
+    }
+}
